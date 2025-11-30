53c8cd021cc1f1d9934dd517e1543179d0e70585
omarm <omarmarouf345@gmail.com>
2025-11-30 18:17:21 +0100

    crear entidad cliente

---
+++ src/Cliente.java
@@ -0,0 +1,76 @@
+public class Cliente {
+
+    private String Nombre;
+    private  String Apellidos;
+    private  String Nif;
+    private  String Direccion;
+    private String Localidad;
+    private  String Provincia;
+    private  String Telefono;
+    private  String Email;
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getNif() {
+        return Nif;
+    }
+
+    public void setNif(String nif) {
+        Nif = nif;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getProvincia() {
+        return Provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        Provincia = provincia;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cliente cliente1=new Cliente();
+        //añadir datos
+        cliente1.setTelefono(" ");
+        cliente1.setNombre(" ");
+        cliente1.setApellidos(" ");
+        cliente1.setDireccion(" ");
+        cliente1.setNif(" ");
+        cliente1.setEmail(" ");
+        cliente1.setLocalidad(" ");
+        cliente1.setProvincia(" ");
+
+
+        //visualizar atributos
+        System.out.println(cliente1.getNombre());
+        System.out.println(cliente1.getApellidos());
+        System.out.println(cliente1.getDireccion());
+        System.out.println(cliente1.getEmail());
+        System.out.println(cliente1.getNif());
+        System.out.println(cliente1.getTelefono());
+        System.out.println(cliente1.getLocalidad());
+        System.out.println(cliente1.getProvincia());
+
 
     }
 }
